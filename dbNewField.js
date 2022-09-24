691f432960d6b129f125ada3b85963fda34384ab
aCarinato <alessandro.carinato@gmail.com>
2022-09-24 17:21:01 +0200

    add field script

---
+++ dbNewField.js
@@ -0,0 +1,25 @@
+import User from './models/User.js';
+
+import connectDB from './config/db.js';
+
+connectDB();
+
+const addField = async () => {
+  try {
+    // await User.aggregate([
+    //   { $addFields: { testField: { type: String, default: 'testone' } } },
+    // ]);
+    await User.updateMany({}, { $set: { newField: 'sucaa' } });
+    // await User.updateMany({}, { newField: 'sucaa' });
+    // await User.aggregate([{ $addFields: { testField: 'testaa' } }]);
+    // await User.updateOne(
+    //   { username: 'ale' },
+    //   { $set: { isProfileCompleted: false } }
+    // );
+    console.log('FATTA');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+addField();
