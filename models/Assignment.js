c9e2a39d12af6beaa3822825bf1f01098af27b5d
aCarinato <alessandro.carinato@gmail.com>
2022-12-06 13:37:41 +0100

    update assignment model

--- models/Assignment.js
+++ models/Assignment.js
@@ -1,73 +1,78 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 // A GROUP NEEDS TO HAVE AT LEAST 2 PEOPLE
-const AssignmentSchema = new Schema({
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  headline: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  difficulty: {
-    type: String,
-    required: true,
-  },
-  completionTime: { type: Number }, // n. days
-  topics: [],
-  learning: [],
-  requirements: [
-    {
-      idx: { type: String },
-      label: { type: String },
+const AssignmentSchema = new Schema(
+  {
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-  repo: {
-    type: String,
-  },
-  mockups: [
-    {
-      caption: { type: String },
-      picture: {
-        url: String,
-        public_id: String,
+    headline: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+    },
+    completionTime: { type: Number }, // n. days
+    topics: [],
+    learning: [],
+    requirements: [
+      {
+        idx: { type: String },
+        label: { type: String },
       },
+    ],
+    repo: {
+      type: String,
     },
-  ],
-  maxTeamMemebers: {
-    type: Number,
-    required: true,
-  },
-  idealTeam: [
-    {
-      idx: { type: String },
-      nPeople: { type: Number },
-      role: { type: String },
-    },
-  ],
-  steps: [
-    {
-      n: { type: String },
-      tasks: [
-        {
-          roleId: {
-            type: String,
-          },
-          roleTasks: {
-            type: String,
-          },
+    mockups: [
+      {
+        caption: { type: String },
+        picture: {
+          url: String,
+          public_id: String,
         },
-      ],
+      },
+    ],
+    maxTeamMemebers: {
+      type: Number,
+      required: true,
     },
-  ],
-});
+    idealTeam: [
+      {
+        idx: { type: String },
+        nPeople: { type: Number },
+        role: { type: String },
+      },
+    ],
+    steps: [
+      {
+        n: { type: String },
+        tasks: [
+          {
+            roleId: {
+              type: String,
+            },
+            roleTasks: {
+              type: String,
+            },
+          },
+        ],
+      },
+    ],
+    resources: [],
+    isPublic: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const Assignment = mongoose.model('Assignment', AssignmentSchema);
 export default Assignment;
--- models/Group.js
+++ models/Group.js
@@ -1,54 +1,57 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 // A GROUP NEEDS TO HAVE AT LEAST 2 PEOPLE
-const GroupSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  deadline: { type: Date },
-  organiser: { type: Schema.Types.ObjectId, ref: 'User' },
-  nBuddies: { type: Number, required: true },
-  buddies: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  buddiesFilled: { type: Boolean, default: false },
-  mentorRequired: { type: Boolean, default: false },
-  nMentorsRequired: { type: Number, default: 1 },
-  mentors: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  mentorsFilled: { type: Boolean, default: false },
-  topics: [],
-  learning: [],
-  hasProposedAssignment: { type: Boolean, default: false },
-  proposedAssignment: {
-    type: Schema.Types.ObjectId,
-    ref: 'Assignment',
-    // default: '',
-  },
-  //   proposedProjectLink: '',
-
-  requirements: [
-    // {
-    //   idx: { type: String },
-    //   label: { type: String },
-    //   met: { type: Boolean, default: false },
-    // },
-  ],
-  approvals: [
-    {
-      participant: { type: Schema.Types.ObjectId, ref: 'User' },
-      approved: { type: Boolean, default: false },
+const GroupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
+    description: {
+      type: String,
+      required: true,
+    },
+    deadline: { type: Date },
+    organiser: { type: Schema.Types.ObjectId, ref: 'User' },
+    nBuddies: { type: Number, required: true },
+    buddies: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    buddiesFilled: { type: Boolean, default: false },
+    mentorRequired: { type: Boolean, default: false },
+    nMentorsRequired: { type: Number, default: 1 },
+    mentors: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    mentorsFilled: { type: Boolean, default: false },
+    topics: [],
+    learning: [],
+    hasProposedAssignment: { type: Boolean, default: false },
+    proposedAssignment: {
+      type: Schema.Types.ObjectId,
+      ref: 'Assignment',
+      // default: '',
+    },
+    //   proposedProjectLink: '',
 
-  isClosed: { type: Boolean, default: false },
-  picture: {
-    url: String,
-    public_id: String,
+    requirements: [
+      // {
+      //   idx: { type: String },
+      //   label: { type: String },
+      //   met: { type: Boolean, default: false },
+      // },
+    ],
+    approvals: [
+      {
+        participant: { type: Schema.Types.ObjectId, ref: 'User' },
+        approved: { type: Boolean, default: false },
+      },
+    ],
+
+    isClosed: { type: Boolean, default: false },
+    picture: {
+      url: String,
+      public_id: String,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Group = mongoose.model('Group', GroupSchema);
 export default Group;
